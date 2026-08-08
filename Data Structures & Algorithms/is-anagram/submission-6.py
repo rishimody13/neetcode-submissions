582da915560a8fad47bfe5f6d0d8cb935cdb454e
Rishi Mody <67372250+rishimody13@users.noreply.github.com>
2026-08-09 01:01:33 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        dict_s = {}
+        dict_t={}
+        for i in range(len(s)):
+            if s[i] not in dict_s:
+                dict_s[s[i]] = 0
+            if t[i] not in dict_t:
+                dict_t[t[i]] = 0
+            dict_s[s[i]]+=1
+            dict_t[t[i]]+=1
+        return dict_s == dict_t
